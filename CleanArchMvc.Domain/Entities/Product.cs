e9347fb80cb92ec2d5b0ad7f0961af260bc72c2a
fabiovalinhos <fabiovalinhos@gmail.com>
2024-08-25 12:30:21 -0300

    Criando o teste para NullReferenceException e deixando a variável image com operador '?'

--- CleanArchMvc.Domain/Entities/Product.cs
+++ CleanArchMvc.Domain/Entities/Product.cs
@@ -83,7 +83,7 @@ namespace CleanArchMvc.Domain.Entities
             DomainExceptionValidation.When(stock < 0,
             "Invalid stock value");
 
-            DomainExceptionValidation.When(image.Length > 250,
+            DomainExceptionValidation.When(image?.Length > 250,
                         "Invalid image name, too long. Maximum 250 characters");
 
             Name = name;
