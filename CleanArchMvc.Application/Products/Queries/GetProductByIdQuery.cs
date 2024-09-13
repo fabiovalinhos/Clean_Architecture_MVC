3d08883f8b178f99bc62ad45a09eea6f20b55b63
fabiovalinhos <fabiovalinhos@gmail.com>
2024-09-12 21:17:09 -0300

    CQRS - implementando comando e consultas para o Product

---
+++ CleanArchMvc.Application/Products/Queries/GetProductByIdQuery.cs
@@ -0,0 +1,16 @@
+using CleanArchMvc.Domain.Entities;
+using MediatR;
+
+namespace CleanArchMvc.Application.Products.Queries
+{
+    public class GetProductByIdQuery : IRequest<Product>
+    {
+        public int Id { get; set; }
+        
+        public GetProductByIdQuery(int id)
+        {
+            this.Id = id;
+
+        }
+    }
+}
