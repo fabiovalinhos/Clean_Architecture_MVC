9eb595419147409544e630eb4a73542bb5cee9cf
fabiovalinhos <fabiovalinhos@gmail.com>
2024-09-28 18:15:13 -0300

    Criação de um Produto

--- CleanArchMvc.WebUI/Controllers/ProductsController.cs
+++ CleanArchMvc.WebUI/Controllers/ProductsController.cs
@@ -1,5 +1,7 @@
+using CleanArchMvc.Application.DTOs;
 using CleanArchMvc.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace CleanArchMvc.WebUI.Controllers
 {
@@ -7,10 +9,14 @@ namespace CleanArchMvc.WebUI.Controllers
     public class ProductsController : Controller
     {
         public readonly IProductService _productService;
+        public readonly ICategoryService _categoryService;
 
-        public ProductsController(IProductService productService)
+        public ProductsController(
+            IProductService productService,
+            ICategoryService categoryService)
         {
             _productService = productService;
+            _categoryService = categoryService;
         }
 
         [HttpGet]
@@ -18,6 +24,31 @@ namespace CleanArchMvc.WebUI.Controllers
         {
             var products = await _productService.GetProducts();
             return View(products);
+        }
+
+        [HttpGet("create")]
+        public async Task<IActionResult> Create()
+        {
+            ViewBag.CategoryId =
+            new SelectList(await _categoryService.GetCategories(), "Id", "Name");
+
+            return View();
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> Create(ProductDTO productDTO)
+        {
+            if (ModelState.IsValid)
+            {
+
+
+
+                
+                await _productService.Add(productDTO);
+                return RedirectToAction(nameof(Index));
+            }
+
+            return View(productDTO);
         }
     }
 }
